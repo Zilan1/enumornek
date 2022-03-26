0ac77071b5a6827f0a1a583e21df214f6b949cd8
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:54:18 +0300

    sum

---
+++ proje82.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ enum sehirler
+	{
+		hatali,mardin,izmir,amasya,bilecik,kocaeli,afyon,agri,tunceli,konya,igdir
+	};
+   
+   int main() {
+	
+	enum sehirler il;
+	il=mardin;
+	printf("%d",il);
+	
+	
+    //******************************************************
+	
+	//char kitapad[30],yazar[30],kitapyazar[60];
+	
+	//printf("Kitap Adini ve Yazari Giriniz:");
+	//scanf("%s%s",kitapad,yazar);
+	
+	//strcat(kitapyazar,kitapad);
+	//strcat(kitapyazar," ");
+	//strcat(kitapyazar,"<-->");
+	//strcat(kitapyazar,yazar);
+	
+	//printf("\n\n");
+	
+	//printf("%s",kitapyazar);
+ 
+ 
+ 
+ //**************************************************************************************
+ 
+ //typedef int d1;
+   
+   //int main(){
+   	
+   	//int toplam;
+   	//d1 d2;
+   	//d1 d3;
+   	//d2=20;
+   	//d3=40;
+   	//toplam=d2+d3;
+   	//printf("%d",toplam);
+   	
+   
+    
+//**************************************************************************************
+    //typedef int tamsayi;
+    
+//int main() {
+	
+	//tamsayi ts;
+	//ts=22;
+	//tamsayi ts2,ts3;
+	//ts2=23;
+	//ts3=24;
+	//printf("%d %d %d",ts,ts2,ts3);
+
+
+
+//***************************************************************************************    
+	//struct kayit
+	//{
+		//char ad[20];
+		//int no;
+		//int sinif;
+		//float ort;
+	//};
+	//struct kayit ogr;
+	
+//int main(){
+	//printf("Ad-Soyad; ");
+	//scanf("%s",&ogr.ad);
+	
+	//printf("Numara: ");
+	//scanf("%d",&ogr.no);
+	
+	//printf("Sinif: ");
+	//scanf("%d",&ogr.sinif);
+	
+	//printf("Ortalama: ");
+	//scanf("%f",&ogr.ort);
+	
+	
+	//printf("%s\n",ogr.ad);
+	//printf("%d\n",ogr.no);
+	//printf("%d\n",ogr.sinif);
+	//printf("%f\n",ogr.ort);
+	
+	//return 0;
+}	
+	
+	
+	
+//********************************************************************************************************************
+	
+	
+	//struct kitapbilgi
+	//{
+		//char kitapad[30];
+		//char yazar[30];
+		//int fiyat;
+		//float puan;
+	//};
+
+//int main() {
+	
+	//struct kitapbilgi kb={"SekerPortakali","Vasconceulos",10,7.25};
+	
+	//printf("Kitap Ad: %s\n",kb.kitapad);
+	
+	//printf("Yazar: %s\n",kb.yazar);
+	
+	//printf("Fiyat: %d\n",kb.fiyat);
+	
+	//printf("Puan: %.2f",kb.puan);
+	
+	
+//}
